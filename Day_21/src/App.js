9e59ac70572fc8ce69f0da8a636b13c7ff7924c0
Utkarsh Vijay <utkarshvijay99@gmail.com>
2021-07-17 01:40:01 +0530

    adding day 22

--- Day_21/src/App.js
+++ Day_21/src/App.js
@@ -1,11 +1,62 @@
-import './App.css';
-import { Addcalorie } from "./components/Addcalorie"
+import "./App.css";
+import AddItemForm from "./components/Addcalorie";
+import { useState } from "react";
 
+function RenderItem({ updateItem, removeItem, item, index }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <div className="card w-50 p-2 my-2" style={{ width: "18rem" }}>
+      <div className="card-body">
+        {isEditing ? (
+          <input
+            type="text"
+            name="title"
+            id="title"
+            required
+            // value={item.title}
+            onChange={(e) => updateItem(index, { title: e.target.value })}
+          />
+        ) : (
+          {/* <h5 className="card-title">{item.title}</h5> */}
+          <h4>Hello</h4>
+        )}
+        {isEditing ? (
+          <input
+            type="number"
+            name="calorie"
+            id="calorie"
+            required
+            // value={item.calorie}
+            onChange={(e) => updateItem(index, { calorie: e.target.value })}
+          />
+        ) : (
+          <p className="='card-text'">
+            {" "}
+            You have consumed {item.calorie} calories
+          </p>
+        )}
+        <div className="d-flex ">
+          <button className="btn btn-danger" onClick={() => removeItem(index)}>
+            Delete
+          </button>
+          <button
+            className="btn mx-3 btn-primary"
+            onClick={() => setIsEditing(!isEditing)}
+          >
+            {isEditing ? "Done" : "Edit"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <Addcalorie/>
+      <AddItemForm />
+      <RenderItem />
     </div>
   );
 }
--- Day_21/src/components/Addcalorie.js
+++ Day_21/src/components/Addcalorie.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import './Addcalorie.css'
+import React, {useState} from 'react'
 
-const handleSubmit = e => {
-    e.preventDefault();
-    addItem({ title, calorie });
-    setTitle('');
-    setCalorie('');
-  };
-  
-export const Addcalorie = () => {
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="container">
-                    <div className="inputs">
-                        <input type="text" />
-                        <input type="text"/>
-                    </div>
-                    
-                    <input type="submit"/>
-                </div>
-            </form>
-        </div>
-    )
+export default function AddItemForm({ addItem }) {
+	const [title, setTitle] = useState('');
+	const [calorie, setCalorie] = useState('');
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		addItem({ title, calorie });
+		setTitle('');
+		setCalorie('');
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit}
+			className='d-flex flex-column justify-content-center my-3 mx-auto'>
+			<div className='d-flex justify-content-center mb-2 '>
+				<div className='me-3'>
+					<label htmlFor='title' className='form-label visually-hidden'>
+						Title
+					</label>
+					<input
+						type='text'
+						name='title'
+						id='title'
+						required
+						value={title}
+						onChange={e => setTitle(e.target.value)}
+						placeholder='Item name'
+					/>
+				</div>
+				<div>
+					<label htmlFor='calorie' className='form-label visually-hidden'>
+						Calorie
+					</label>
+					<input
+						type='number'
+						name='number'
+						id='number'
+						required
+						value={calorie}
+						onChange={e => setCalorie(e.target.value)}
+						placeholder='Calorie amount'
+					/>
+				</div>
+			</div>
+			<button type='submit' className='btn btn-primary mx-auto'>
+				Add Item
+			</button>
+		</form>
+	);
 }
