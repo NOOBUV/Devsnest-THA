a17b10548d1603e31ea7347e5b81f3d97ce91220
Utkarsh Vijay <utkarshvijay99@gmail.com>
2021-09-18 16:52:47 +0530

    some changes + added day 11

--- Backend/Day_09/routes/users.js
+++ Backend/Day_09/routes/users.js
@@ -1,37 +1,14 @@
-var express = require('express');
-const fs = require('fs')
-const path = require('path');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-
-
-router.get('/file/:filename(*$)', function (req, res) {
-  const filename = req.params.filename
-  const fullpath = path.join(__dirname, "../public/images")
-  const abspath = path.join(fullpath, filename)
-
-  if (fs.statSync(abspath).isFile()) {                // checks if it is a folder or a file
-    if (fs.existsSync(path.join(fullpath, filename))) //prevents from routes being visible
-      res.sendFile(path.join(fullpath, filename));
-    else
-      res.sendStatus(404)
-    // res.send('respond with a resource');
-  } else {
-    res.sendStatus(404).send('it is a folder')
-  }
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
-
-router.get('/file', function (req, res) {
-  res.send('respond with a resource');
-
+router.get("/mango/:name", function (req, res, next) {
+  // res.send('respond with a resource');
+  console.log("hello");
+  res.sendFile(path.join(__dirname, "../stylesheets/style.css"), "style.css");
 });
-
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
-
-
-
 
 module.exports = router;
